793beee178d4c04fcc075f21f80b5133bb04db05
LecherousMan <121013939+LecherousMan@users.noreply.github.com>
2024-10-23 21:38:53 +0800

    Update api.js

--- recaptcha/api.js
+++ recaptcha/api.js
@@ -1,27 +1,28 @@
-/* PLEASE DO NOT COPY AND PASTE THIS CODE. */
 (function() {
-    var w = window,
-    C = '___grecaptcha_cfg',
-    cfg = w[C] = w[C] || {},
-    N = 'grecaptcha';
+    var w = window;
+    var d = document;
+    var s = d.createElement('script');
+    
+    // 使用Google通用的reCAPTCHA API地址，自动加载最新的版本
+    s.src = 'https://recaptcha.net/recaptcha/api.js';  // 如果需要，可以替换为 'https://recaptcha.net/recaptcha/api.js'
+    s.async = true;
+    s.defer = true;  // 确保脚本延迟加载
+    var t = d.getElementsByTagName('script')[0];
+    t.parentNode.insertBefore(s, t);
+
+    // 初始化 reCAPTCHA 配置
+    var C = '___grecaptcha_cfg';
+    var cfg = w[C] = w[C] || {};
+    var N = 'grecaptcha';
     var gr = w[N] = w[N] || {};
-    gr.ready = gr.ready ||
-    function(f) { (cfg['fns'] = cfg['fns'] || []).push(f);
+
+    // ready 函数将传入的函数添加到 grecaptcha 的回调队列中
+    gr.ready = gr.ready || function(f) {
+        (cfg['fns'] = cfg['fns'] || []).push(f);
     };
-    w['__recaptcha_api'] = 'https://recaptcha.net/recaptcha/api2/'; (cfg['render'] = cfg['render'] || []).push('onload');
+
+    // 设置 reCAPTCHA API URL
+    w['__recaptcha_api'] = 'https://recaptcha.net/recaptcha/api2/';
+    (cfg['render'] = cfg['render'] || []).push('onload');
     w['__google_recaptcha_client'] = true;
-    var d = document,
-    po = d.createElement('script');
-    po.type = 'text/javascript';
-    po.async = true;
-    po.src = 'https://www.gstatic.com/recaptcha/releases/pxZcVU8Dk73FyvFvdCgp2MSG/recaptcha__en.js';
-    po.crossOrigin = 'anonymous';
-    po.integrity = 'sha384-Lo3ZXU7l5KjVR72cD8bbMsXArkkXuD31OpIB/WF4jL1Ck1rSPx8ijndY5XejGW85';
-    var e = d.querySelector('script[nonce]'),
-    n = e && (e['nonce'] || e.getAttribute('nonce'));
-    if (n) {
-        po.setAttribute('nonce', n);
-    }
-    var s = d.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(po, s);
 })();
